868e6859d5e5d2e06ba091a99c2991b72463745c
Daniel Derstappen <daniel.derstappen@dermalog.com>
2021-05-20 08:57:38 +0200

    Replace tabs with spaces

--- plugins/music_service/welle-cli/index.js
+++ plugins/music_service/welle-cli/index.js
@@ -1,49 +1,48 @@
 'use strict';
 
 var libQ = require('kew');
-var fs=require('fs-extra');
+var fs = require('fs-extra');
 var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
 
-module.exports = welle-cli;
-function welle-cli(context) {
-	var self = this;
+module.exports = welle_cli;
+function welle_cli(context) {
+    var self = this;
 
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
+    this.context = context;
+    this.commandRouter = this.context.coreCommand;
+    this.logger = this.context.logger;
+    this.configManager = this.context.configManager;
 
 }
 
 
 
-welle-cli.prototype.onVolumioStart = function()
-{
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+welle_cli.prototype.onVolumioStart = function () {
+    var self = this;
+    var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 }
 
-welle-cli.prototype.onStart = function() {
+welle_cli.prototype.onStart = function () {
     var self = this;
-	var defer=libQ.defer();
+    var defer = libQ.defer();
 
 
-	// Once the Plugin has successfull started resolve the promise
-	defer.resolve();
+    // Once the Plugin has successfull started resolve the promise
+    defer.resolve();
 
     return defer.promise;
 };
 
-welle-cli.prototype.onStop = function() {
+welle_cli.prototype.onStop = function () {
     var self = this;
-    var defer=libQ.defer();
+    var defer = libQ.defer();
 
     // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
@@ -51,7 +50,7 @@ welle-cli.prototype.onStop = function() {
     return libQ.resolve();
 };
 
-welle-cli.prototype.onRestart = function() {
+welle_cli.prototype.onRestart = function () {
     var self = this;
     // Optional, use if you need it
 };
@@ -59,46 +58,44 @@ welle-cli.prototype.onRestart = function() {
 
 // Configuration Methods -----------------------------------------------------------------------------
 
-welle-cli.prototype.getUIConfig = function() {
+welle_cli.prototype.getUIConfig = function () {
     var defer = libQ.defer();
     var self = this;
 
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-        __dirname+'/i18n/strings_en.json',
+    self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+        __dirname + '/i18n/strings_en.json',
         __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
+        .then(function (uiconf) {
 
 
             defer.resolve(uiconf);
         })
-        .fail(function()
-        {
+        .fail(function () {
             defer.reject(new Error());
         });
 
     return defer.promise;
 };
 
-welle-cli.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
+welle_cli.prototype.getConfigurationFiles = function () {
+    return ['config.json'];
 }
 
-welle-cli.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+welle_cli.prototype.setUIConfig = function (data) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
-welle-cli.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+welle_cli.prototype.getConf = function (varName) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
-welle-cli.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
+welle_cli.prototype.setConf = function (varName, varValue) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
 
@@ -107,14 +104,14 @@ welle-cli.prototype.setConf = function(varName, varValue) {
 // If your plugin is not a music_sevice don't use this part and delete it
 
 
-welle-cli.prototype.addToBrowseSources = function () {
+welle_cli.prototype.addToBrowseSources = function () {
 
-	// Use this function to add your music service plugin to music sources
+    // Use this function to add your music service plugin to music sources
     //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
     this.commandRouter.volumioAddToBrowseSources(data);
 };
 
-welle-cli.prototype.handleBrowseUri = function (curUri) {
+welle_cli.prototype.handleBrowseUri = function (curUri) {
     var self = this;
 
     //self.commandRouter.logger.info(curUri);
@@ -127,141 +124,141 @@ welle-cli.prototype.handleBrowseUri = function (curUri) {
 
 
 // Define a method to clear, add, and play an array of tracks
-welle-cli.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::clearAddPlayTrack');
+welle_cli.prototype.clearAddPlayTrack = function (track) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::clearAddPlayTrack');
 
-	self.commandRouter.logger.info(JSON.stringify(track));
+    self.commandRouter.logger.info(JSON.stringify(track));
 
-	return self.sendSpopCommand('uplay', [track.uri]);
+    return self.sendSpopCommand('uplay', [track.uri]);
 };
 
-welle-cli.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::seek to ' + timepos);
+welle_cli.prototype.seek = function (timepos) {
+    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::seek to ' + timepos);
 
-    return this.sendSpopCommand('seek '+timepos, []);
+    return this.sendSpopCommand('seek ' + timepos, []);
 };
 
 // Stop
-welle-cli.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::stop');
+welle_cli.prototype.stop = function () {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::stop');
 
 
 };
 
 // Spop pause
-welle-cli.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::pause');
+welle_cli.prototype.pause = function () {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::pause');
 
 
 };
 
 // Get state
-welle-cli.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::getState');
+welle_cli.prototype.getState = function () {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::getState');
 
 
 };
 
 //Parse state
-welle-cli.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::parseState');
+welle_cli.prototype.parseState = function (sState) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::parseState');
 
-	//Use this method to parse the state and eventually send it with the following function
+    //Use this method to parse the state and eventually send it with the following function
 };
 
 // Announce updated State
-welle-cli.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle-cli::pushState');
+welle_cli.prototype.pushState = function (state) {
+    var self = this;
+    self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'welle_cli::pushState');
 
-	return self.commandRouter.servicePushState(state, self.servicename);
+    return self.commandRouter.servicePushState(state, self.servicename);
 };
 
 
-welle-cli.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
+welle_cli.prototype.explodeUri = function (uri) {
+    var self = this;
+    var defer = libQ.defer();
 
-	// Mandatory: retrieve all info for a given URI
+    // Mandatory: retrieve all info for a given URI
 
-	return defer.promise;
+    return defer.promise;
 };
 
-welle-cli.prototype.getAlbumArt = function (data, path) {
+welle_cli.prototype.getAlbumArt = function (data, path) {
 
-	var artist, album;
+    var artist, album;
 
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
+    if (data != undefined && data.path != undefined) {
+        path = data.path;
+    }
 
-	var web;
+    var web;
 
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
+    if (data != undefined && data.artist != undefined) {
+        artist = data.artist;
+        if (data.album != undefined)
+            album = data.album;
+        else album = data.artist;
 
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
+        web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
+    }
 
-	var url = '/albumart';
+    var url = '/albumart';
 
-	if (web != undefined)
-		url = url + web;
+    if (web != undefined)
+        url = url + web;
 
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
+    if (web != undefined && path != undefined)
+        url = url + '&';
+    else if (path != undefined)
+        url = url + '?';
 
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
+    if (path != undefined)
+        url = url + 'path=' + nodetools.urlEncode(path);
 
-	return url;
+    return url;
 };
 
 
 
 
 
-welle-cli.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
+welle_cli.prototype.search = function (query) {
+    var self = this;
+    var defer = libQ.defer();
 
-	// Mandatory, search. You can divide the search in sections using following functions
+    // Mandatory, search. You can divide the search in sections using following functions
 
-	return defer.promise;
+    return defer.promise;
 };
 
-welle-cli.prototype._searchArtists = function (results) {
-
-};
-
-welle-cli.prototype._searchAlbums = function (results) {
+welle_cli.prototype._searchArtists = function (results) {
 
 };
 
-welle-cli.prototype._searchPlaylists = function (results) {
+welle_cli.prototype._searchAlbums = function (results) {
+
+};
+
+welle_cli.prototype._searchPlaylists = function (results) {
 
 
 };
 
-welle-cli.prototype._searchTracks = function (results) {
+welle_cli.prototype._searchTracks = function (results) {
 
 };
 
-welle-cli.prototype.goto=function(data){
-    var self=this
-    var defer=libQ.defer()
+welle_cli.prototype.goto = function (data) {
+    var self = this
+    var defer = libQ.defer()
 
-// Handle go to artist and go to album function
+    // Handle go to artist and go to album function
 
-     return defer.promise;
+    return defer.promise;
 };
